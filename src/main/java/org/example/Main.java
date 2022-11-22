891eb81149d93de9ebff05b341cd4ce7b6d8f401
OGrant <OmariGrant@users.noreply.github.com>
2022-11-22 19:04:33 +0000

    Added missing values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+       for (Planet planet: Planet.values()){
+           boolean checkHash =  Planet.EARTH.hashCode() == planet.hashCode();
+           
+           System.out.println("Name: " + planet.getName());
+           System.out.println("Distance From Sun: " + planet.getDistanceFromSun() + " miles");
+           System.out.println("Mass in KG: " + planet.getMassInKG() + "KG");
+           System.out.println("Same Object as Earth?: " + Planet.EARTH.equals(planet));
+           System.out.println("Same Memory Address as Earth?: " + checkHash);
+           System.out.println("Days for one year orbit: " + planet.getYearOrbitInEarthDays());
+           System.out.println("----------------------");
+       }
 
         if(Planet.MARS.getDistanceFromSun() != Planet.EARTH.getDistanceFromSun()){
             System.out.println("Not the same distance");
--- src/main/java/org/example/Planet.java
+++ src/main/java/org/example/Planet.java
@@ -1,14 +1,52 @@
 package org.example;
 
 public enum Planet {
-    MERCURY("Mercury", 35.98  * MathValues.MILLION.getValue(),(MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 0.055, 0),
-    VENUS("Venus", 67.24 * MathValues.MILLION.getValue(), (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 0.815, 0),
-    EARTH("Earth", 91.00 * MathValues.MILLION.getValue(), (5972190000000000L  * MathValues.BILLION.getValue()), 0),
-    MARS("Mars", 141.6 * MathValues.MILLION.getValue(), (5972190000000000L  * MathValues.BILLION.getValue()) * 0.107, 0),
-    JUPITER("Jupiter", 43.44 * MathValues.MILLION.getValue(), (5972190000000000L  * MathValues.BILLION.getValue()) * 317.83, 0),
-    SATURN("Saturn", EARTH.distanceFromSun * 9.5, (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 95.16, 0),
-    URANUS("Uranus", 1.8 * MathValues.BILLION.getValue(), (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 14.536, 0),
-    NEPTUNE("Neptune", 2.8 * MathValues.BILLION.getValue(), (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 17.15 , 0);
+    MERCURY(
+            "Mercury",
+            35.98  * MathValues.MILLION.getValue(),
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 0.055,
+            87.97
+    ),
+    VENUS(
+            "Venus",
+            67.24 * MathValues.MILLION.getValue(),
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 0.815,
+            224.70),
+    EARTH(
+            "Earth",
+            91.00 * MathValues.MILLION.getValue(),
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()),
+            365.26
+    ),
+    MARS(
+            "Mars",
+            141.6 * MathValues.MILLION.getValue(),
+            (5972190000000000L  * MathValues.BILLION.getValue()) * 0.107,
+            686.98
+    ),
+    JUPITER(
+            "Jupiter",
+            43.44 * MathValues.MILLION.getValue(),
+            (5972190000000000L  * MathValues.BILLION.getValue()) * 317.83,
+            4332.82
+    ),
+    SATURN(
+            "Saturn", EARTH.distanceFromSun * 9.5,
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 95.16,
+            10755.70
+    ),
+    URANUS(
+            "Uranus",
+            1.8 * MathValues.BILLION.getValue(),
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 14.536,
+            30687.15
+    ),
+    NEPTUNE(
+            "Neptune",
+            2.8 * MathValues.BILLION.getValue(),
+            (MathValues.EARTH_SIZE.getLongValue() * MathValues.BILLION.getValue()) * 17.15,
+            60190.03
+    );
 
     private final String name;
     private final double distanceFromSun;
