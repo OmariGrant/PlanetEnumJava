34c5ba4e62f1614efe1681c67bb4904468da8f04
OGrant <OmariGrant@users.noreply.github.com>
2022-11-22 19:08:58 +0000

    demo code for Planets enum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,23 @@ public class Main {
     public static void main(String[] args) {
 
        for (Planet planet: Planet.values()){
-           boolean checkHash =  Planet.EARTH.hashCode() == planet.hashCode();
-           
+           boolean checkHashEarth =  Planet.EARTH.hashCode() == planet.hashCode();
+           boolean checkHashMars =  Planet.MARS.hashCode() == planet.hashCode();
+
            System.out.println("Name: " + planet.getName());
            System.out.println("Distance From Sun: " + planet.getDistanceFromSun() + " miles");
            System.out.println("Mass in KG: " + planet.getMassInKG() + "KG");
-           System.out.println("Same Object as Earth?: " + Planet.EARTH.equals(planet));
-           System.out.println("Same Memory Address as Earth?: " + checkHash);
            System.out.println("Days for one year orbit: " + planet.getYearOrbitInEarthDays());
+           System.out.println("***** Programming Info *****");
+           System.out.println("Same Object as Earth?: " + Planet.EARTH.equals(planet));
+           System.out.println("Same Object as Mars?: " + Planet.MARS.equals(planet));
+           System.out.println("Same Object as Venus?: " + Planet.VENUS.equals(planet));
+           System.out.println("Same Object as Saturn?: " + Planet.SATURN.equals(planet));
+           System.out.println("Same Object as Jupiter?: " + Planet.JUPITER.equals(planet));
+           System.out.println("Same Memory Address as Earth?: " + checkHashEarth);
+           System.out.println("Same Memory Address as Mars?: " + checkHashMars);
            System.out.println("----------------------");
        }
-
-        if(Planet.MARS.getDistanceFromSun() != Planet.EARTH.getDistanceFromSun()){
-            System.out.println("Not the same distance");
-        }
-
 
     }
 }
